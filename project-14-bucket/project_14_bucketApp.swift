c5c38042338f9dbb4f0ae99b40f88883277ffe5b
adarsh <adarsh0ranjan@gmail.com>
2022-08-19 10:34:43 +0530

    Initial Commit

---
+++ project-14-bucket/project_14_bucketApp.swift
@@ -0,0 +1,17 @@
+//
+//  project_14_bucketApp.swift
+//  project-14-bucket
+//
+//  Created by Roro Solutions on 19/08/22.
+//
+
+import SwiftUI
+
+@main
+struct project_14_bucketApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
